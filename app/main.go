ef3c59ff6330e70db444098faa72c7cc758e48da
Thomas Huang <thomaskhuang@ucla.edu>
2026-05-28 15:29:17 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,17 +12,25 @@ func main() {
 		fmt.Print("$ ")
 
 		reader := bufio.NewReader(os.Stdin)
-		command, err := reader.ReadString('\n')
+		userInput, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error reading input:", err)
 			break
 		}
 
-		command = strings.TrimRight(command, "\r\n")
-		if command == "exit" {
-			break
+		parts := strings.SplitN(strings.TrimRight(userInput, "\r\n"), " ", 2)
+		cmd, args := parts[0], ""
+		if len(parts) > 1 {
+			args = parts[1]
 		}
 
-		fmt.Printf("%s: command not found\n", command)
+		switch cmd {
+		case "exit":
+			return
+		case "echo":
+			fmt.Println(args)
+		default:
+			fmt.Printf("%s: command not found\n", cmd)
+		}
 	}
 }
