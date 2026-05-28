6c109a964f98e99e23a669fc0dc0ef8d349ca657
Thomas Huang <thomaskhuang@ucla.edu>
2026-05-28 15:29:16 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,19 +7,18 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
 
-	reader := bufio.NewReader(os.Stdin)
-	command, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		os.Exit(1)
+		reader := bufio.NewReader(os.Stdin)
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			break
+		}
+
+		command = strings.TrimRight(command, "\r\n")
+		fmt.Printf("%s: command not found\n", command)
 	}
-
-	command = strings.TrimRight(command, "\r\n")
-	fmt.Printf("%s: command not found\n", command)
 }
