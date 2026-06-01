d68b1f293483432eaa36d97fb85bd22ee2800ead
Thomas Huang <thomaskhuang@ucla.edu>
2026-06-01 09:47:16 -0700

    codecrafters submit [skip ci]

--- app/inputparser.go
+++ app/inputparser.go
@@ -2,39 +2,63 @@ package main
 
 import "strings"
 
+var specialCharacters = map[rune]bool{
+	'\\': true,
+	'"':  true,
+	'\'': true,
+	'$':  true,
+	'`':  true,
+	'\n': true,
+}
+
 func parseUserInput(input string) []string {
 	var parts []string
 
-	var currWord strings.Builder
+	var currPart strings.Builder
 	quoteChar := rune(0) // 0 means not in quotes
 	for i := 0; i < len(input); {
 		c := rune(input[i])
 
 		switch {
-		case quoteChar == 0 && c == '\\': // if we encounter a backslash, we skip the next character and add it to the current word
-			i++
-			if i < len(input) {
-				c = rune(input[i])
-				currWord.WriteRune(c)
+		case c == '\\': // if we encounter a backslash, we skip the next character and add it to the current word
+			if i+1 < len(input) {
+				next := rune(input[i+1])
+				switch quoteChar {
+				case '"':
+					if specialCharacters[next] {
+						currPart.WriteRune(next)
+					} else {
+						currPart.WriteRune('\\')
+						currPart.WriteRune(next)
+					}
+				case 0:
+					currPart.WriteRune(next)
+				default:
+					currPart.WriteRune('\\')
+					currPart.WriteRune(next)
+				}
+				i++ // skip the escaped character
+			} else {
+				currPart.WriteRune('\\')
 			}
 		case quoteChar == 0 && (c == '"' || c == '\''): // whenever we encounter a double quote, we toggle whether we're within quotes or not
 			quoteChar = c
 		case c == quoteChar:
 			quoteChar = 0
 		case c == ' ' && quoteChar == 0:
-			if currWord.Len() > 0 {
-				parts = append(parts, currWord.String())
-				currWord.Reset()
+			if currPart.Len() > 0 {
+				parts = append(parts, currPart.String())
+				currPart.Reset()
 			}
 		default:
-			currWord.WriteRune(c)
+			currPart.WriteRune(c)
 		}
 
 		i++
 	}
 
-	if currWord.Len() > 0 {
-		parts = append(parts, currWord.String())
+	if currPart.Len() > 0 {
+		parts = append(parts, currPart.String())
 	}
 
 	return parts
